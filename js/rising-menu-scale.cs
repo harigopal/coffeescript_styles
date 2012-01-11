727434dca68bdf13ba7256193eb5d4d7fcd8374a
Hari Gopal <harigopal1@gmail.com>
2012-01-11 14:07:35 +0530

    Adding a variation of the menu with the 'rising' background, introducting a scale to all options.

---
+++ js/rising-menu-scale.cs
@@ -0,0 +1,20 @@
+$ ->
+  $("a.arsm").hover(
+    (->
+      option_number = $(this).attr("id")[4..5]
+      #space_id = "div#rsin#{option_number}"
+      #$(space_id).animate { "height": "0px" }
+      for id in [1..5]
+        do (id) ->
+          space_id = "div#rsin#{id}"
+          height = 100 - Math.round(100 / (Math.pow(2, (Math.abs(option_number - id)))))
+          $(space_id).animate { "height": "#{height}px" }
+    ), (->))
+  $("ul#rs").hover(null,
+    (->
+      for id in [1..5]
+        do (id) ->
+          space_id = "div#rsin#{id}"
+          $(space_id).animate { "height": "100px" }
+    )
+  )
